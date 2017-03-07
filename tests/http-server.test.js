22bb17a440a07fd2c329743c5c9e78c1232327f2
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-07 09:40:46 +0100

    proper test

--- tests/http-server.test.js
+++ tests/http-server.test.js
@@ -3,11 +3,14 @@ const request = require('supertest')
 
 describe('aufgabe 4', () => {
   it('sollte hello world im browser ausgeben', done => {
-    return request(s)
-      .get('/')
-      .expect(200)
-      .then(response => {
-        expect(response.body).toEqual('hello world')
-      })
+    it('sollte hello world im browser ausgeben', done => {
+      return request(s)
+        .get('/')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.text).toEqual('hello world')
+          done()
+        })
+    })
   })
 })
