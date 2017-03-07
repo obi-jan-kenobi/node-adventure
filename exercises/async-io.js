903c886e78e65c1482a065c83fb382d59025b11e
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-07 09:24:17 +0100

    async-io solution

--- exercises/async-io.js
+++ exercises/async-io.js
@@ -1,8 +1,12 @@
 // ihr bekommt nen callback, der mit der anzahl der lines im file
 // aufgerufen werden soll, sobald das file async gelesen wurde.
+const fs = require('fs')
 
 function asyncIo(filepath, cb) {
-
+  fs.readFile(filepath, 'utf-8' , (err, data) => {
+    if (data)
+      cb(data.toString().split('\n').length)
+  })
 }
 
 module.exports = asyncIo
