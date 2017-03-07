0de4534a3416f366f9f5063aa312897e8165a9f7
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-07 09:05:50 +0100

    first-io solution

--- exercises/first-io.js
+++ exercises/first-io.js
@@ -1,3 +1,8 @@
-function firstIo(file) {}
+const fs = require('fs')
+
+function firstIo(filepath) {
+  const file = fs.readFileSync(filepath)
+  return file.toString().split('\n').length
+}
 
 module.exports = firstIo
