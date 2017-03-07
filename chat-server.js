1d72877672a2e7e60031e6fc2be0056e5c22b9c6
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-07 10:47:27 +0100

    finished server

--- chat-server.js
+++ chat-server.js
@@ -1,5 +1,26 @@
 const net = require('net')
 
-const chatServer = null
+const chatServer = net.createServer()
+let clients = []
 
-if (process.env[2] === 3000) chatServer.listen(3000)
+chatServer.on('connection', client => {
+  client.name = `${client.remoteAddress}:${client.remotePort}`
+  clients.push(client)
+  client.write('Hi!\n')
+  client.write('Bye!\n')
+
+  client.on('data', data => {
+    clients
+      .filter(c => c !== client)
+      .forEach(c => {
+      c.write(client.name + " says " + data)
+    })
+  })
+
+  client.on('end', () => {
+    clients = [...clients.slice(0, clients.indexOf(client)), ...clients.slice(clients.indexOf(client) + 1)]
+  })
+
+})
+
+chatServer.listen(9000)
