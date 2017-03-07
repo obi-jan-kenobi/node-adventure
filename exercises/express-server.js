575ef7cff03fba1f69b930acc005ad2e3c99d484
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-07 10:06:51 +0100

    soltion

--- exercises/express-server.js
+++ exercises/express-server.js
@@ -1,6 +1,12 @@
 // express server erstellen, der GET '/atlantec' mit 'finde das mal raus'
 // beantwortet
+const server = require('express')()
 
-const server = null
+server.get('/atlantec', (req, res) => {
+  res.write('finde das mal raus')
+  res.end()
+})
+
+if (process.argv[2] === 3000) server.listen(3000)
 
 module.exports = server
