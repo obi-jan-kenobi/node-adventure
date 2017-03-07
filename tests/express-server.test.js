008565a92a1a52d78cb6bd261a76ec97f6152802
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-03-07 10:03:00 +0100

    test complete

--- tests/express-server.test.js
+++ tests/express-server.test.js
@@ -1,2 +1,14 @@
 const eserv = require('../exercises/express-server')
 const request = require('supertest')
+
+describe('aufgabe 5', () => {
+  it('sollte bei aufruf von /atlantec "finde das mal raus" antworten', done => {
+    return request(eserv)
+      .get('/atlantec')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).toEqual('finde das mal raus')
+        done()
+      })
+  })
+})
